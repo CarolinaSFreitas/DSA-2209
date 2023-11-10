67d421470017a6a12c61b33d2c40973db7bb0d16
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-11-10 10:14:04 -0300

    Add files via upload

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,20 +1,37 @@
+import bcrypt from 'bcrypt'
+
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../database/conecta.js';
 
 export const Usuario = sequelize.define('usuario', {
-  id : {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  nome: {
-    type: DataTypes.STRING(40),
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING(40),
-    allowNull: false
-  }
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    },
+    senha: {
+        type: DataTypes.STRING(60),
+        allowNull: false
+    }
+
 }, {
-  timestamps: false
+    timestamps: false
 });
+
+// hook (gancho) do Sequelize que é executado antes da inserção
+// de um registro. Faz a criptografia da senha
+// e atribui o hash ao campo senha
+Usuario.beforeCreate(usuario => {
+    const salt = bcrypt.genSaltSync(12)
+    const hash = bcrypt.hashSync(usuario.senha, salt)
+    usuario.senha = hash
+})
+
