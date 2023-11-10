4a1d2eee49e4786bb3584ff0b6c0ee2c2813a704
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-11-10 11:10:39 -0300

    Add files via upload

---
+++ controllers/loginController.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken"
+import bcrypt from 'bcrypt'
+
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import { Usuario } from "../models/Usuario.js"
+
+export async function loginUsuario(req, res){
+    
+}
+
+
+
