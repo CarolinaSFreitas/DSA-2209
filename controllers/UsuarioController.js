6da292a8508bbd404e965ca98fbd49ffdb55858b
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-11-10 10:28:46 -0300

    Add files via upload

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -10,12 +10,58 @@ export async function usuarioIndex(req, res) {
     }
 }
 
+function validaSenha(senha) {
+
+    const mensa = []
+    // .length: retorna o tamanho da string (da senha)
+
+    if (senha.length < 8) {
+        mensa.push("Erro... senha deve possuir, no mínimo, 8 caracteres")
+    }
+
+    // contadores
+    let pequenas = 0
+    let grandes = 0
+    let numeros = 0
+    let simbolos = 0
+
+    // senha = "abc123"
+    // letra = "a"
+    // percorre as letras da variável senha
+
+    for (const letra of senha) {
+
+        // expressão regular
+        if ((/[a-z]/).test(letra)) {
+            pequenas++
+        }
+        else if ((/[A-Z]/).test(letra)) {
+            grandes++
+        }
+        else if ((/[0-9]/).test(letra)) {
+            numeros++
+        } else {
+            simbolos++
+        }
+    }
+    if (pequenas == 0 || grandes == 0 || numeros == 0 || simbolos == 0) {
+        mensa.push("Erro... senha deve possuir letras minúsculas, maiúsculas, números e símbolos")
+    }
+    return mensa
+}
+
 // função de create - vai criar um novo registro no insomnia
 export async function usuarioCreate(req, res) {
     const { nome, email, senha } = req.body
 
-    if (!nome || !email || !senha ){
+    if (!nome || !email || !senha) {
         res.status(400).json("Erro... Informe nome, email e senha.")
+        return
+    }
+
+    const mensagem = validaSenha(senha)
+    if (mensagem.length > 0) {
+        res.status(400).json({ erro: mensagem.join(', ') })
         return
     }
 
@@ -28,4 +74,3 @@ export async function usuarioCreate(req, res) {
         res.status(400).send(error)
     }
 }
-
